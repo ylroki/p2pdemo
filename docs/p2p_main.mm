dd860926b12d707696424cf2d4291cb4c0b1e0c1
ylroki-1015PX <leo221221@sina.com>
2012-03-10 21:03:49 +0800

    document update.

---
+++ docs/p2p_main.mm
@@ -0,0 +1,20 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1294123845969" ID="ID_1125410527" LINK="../downloader/network/doc/kugou7-network.mm" MODIFIED="1331384434338" TEXT="p2p main procedure">
+<node CREATED="1331384437318" ID="ID_867771177" MODIFIED="1331384487120" POSITION="right" TEXT="daemonize, and bind a socket listenning local command">
+<node CREATED="1331384492367" ID="ID_1930825075" MODIFIED="1331384506438" TEXT="download">
+<node CREATED="1328357159533" ID="ID_1452656903" MODIFIED="1328357262650" TEXT="a thread run function CDownload::Work()">
+<node CREATED="1331263213440" ID="ID_505015701" MODIFIED="1331263235008" TEXT="a CSourceManager starts">
+<node CREATED="1328357361538" ID="ID_342857623" MODIFIED="1328442725755" TEXT="for each source, update"/>
+</node>
+<node CREATED="1331263372420" ID="ID_274205583" MODIFIED="1331263445185" TEXT="loop,might request source again">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1331263417494" ID="ID_1450072089" MODIFIED="1331263426618" TEXT="stop manager"/>
+</node>
+</node>
+<node CREATED="1331384567726" ID="ID_913106098" MODIFIED="1331384569795" TEXT="stop"/>
+</node>
+<node CREATED="1331384524036" ID="ID_825897321" MODIFIED="1331384548473" POSITION="right" TEXT="a thread run upload file procedure"/>
+</node>
+</map>
